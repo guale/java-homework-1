0c1e64559ce64b8194b52e8da0262c795641f782
guale <gualeshadow@gmail.com>
2021-07-19 11:16:35 -0500

    first commit

---
+++ src/illegalProject.java
@@ -0,0 +1,18 @@
+public class illegalProject {
+    public static void main(String[] args) {
+        String #illegalString = "This is illegal";
+        System.out.print(#illegalString);
+
+        String aString;
+        System.out.print(aString);
+
+        if (true) {
+            int num = 1;
+            System.out.print(num);
+        }
+
+        System.out.print(num);
+    }
+
+
+}
